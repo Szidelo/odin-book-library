27ee8ae8e56f65c5d217a237f16fa8051d73ce55
Szidelo <claudiuszidelo@yahoo.ro>
2025-02-06 09:18:45 +0200

    x

--- script.js
+++ script.js
@@ -7,132 +7,92 @@
 // the book should have the option to remove, to change status and to update the pages read to keep track
 // the in progress books should have a different card for desktop
 
-// declare a constant with three status values
-
 import { createLargeCard } from "./card.js";
 
+// declare a constant with three status values
+
 const STATUS = {
-    QUEUE: "queue",
-    IN_PROGRESS: "in progress",
-    FINISHED: "finished",
+	QUEUE: "queue",
+	IN_PROGRESS: "in progress",
+	FINISHED: "finished",
 };
 
 const inProgressList = document.querySelector(".list");
 
 // create a constructor for the book (can be converted to a class but for learning i will use function constructor and object prototype)
 function Book(title, author, pages, status, pagesRead, category) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.pagesRead = pagesRead || 0;
-    this.category = category;
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.status = status;
+	this.pagesRead = pagesRead || 0;
+	this.category = category;
 }
 
 // prototypes for Books
-Book.prototype.getBookLength = function () {
-    return `The book ${this.title} has ${this.pages}. Yes is a ${
-        this.pages < 300 ? "short" : "long"
-    } book`;
-};
-
 Book.prototype.updateProgress = function (value) {
-    this.pagesRead = value;
-    return this.pagesRead;
+	this.pagesRead = value;
+	return this.pagesRead;
 };
 
 Book.prototype.getPercentRead = function () {
-    if (this.pagesRead === 0) return 0;
-    return ((this.pagesRead / this.pages) * 100).toFixed(2);
+	if (this.pagesRead === 0) return 0;
+	return ((this.pagesRead / this.pages) * 100).toFixed(2);
 };
 
 Book.prototype.updateStatus = function (status) {
-    this.status = status;
+	this.status = status;
 };
 
 // create a constructor for library with list of books and methods
 function Library() {
-    this.bookList = [];
+	this.bookList = [];
 }
 
 // prototypes for Library
 Library.prototype.getBooks = function () {
-    return this.bookList;
+	return this.bookList;
 };
 
 Library.prototype.getCategoryOfBooks = function (category) {
-    return this.bookList.filter((book) => {
-        return book.category.includes(category);
-    });
+	return this.bookList.filter((book) => {
+		return book.category.includes(category);
+	});
 };
 
 Library.prototype.addToBook = function (book) {
-    this.bookList.push(book);
+	this.bookList.push(book);
 };
 
 Library.prototype.removeBook = function (bookToBeRemoved) {
-    const newList = this.bookList.filter((book) => {
-        return book.title !== bookToBeRemoved.title;
-    });
+	const newList = this.bookList.filter((book) => {
+		return book.title !== bookToBeRemoved.title;
+	});
 
-    this.bookList = newList;
+	this.bookList = newList;
 };
 
 Library.prototype.searchBook = function (keyword) {
-    return this.bookList.filter((book) => {
-        return Object.values(book)
-            .join(" ")
-            .toLowerCase()
-            .includes(keyword.toLowerCase());
-    });
+	return this.bookList.filter((book) => {
+		return Object.values(book).join(" ").toLowerCase().includes(keyword.toLowerCase());
+	});
 };
 
 const library = new Library();
 
-const theHobbit = new Book(
-    "The Hobbit",
-    "J.R.R Tolkien",
-    256,
-    STATUS.FINISHED,
-    256,
-    "fantasy"
-);
-const theShining = new Book(
-    "The Shining",
-    "Stephen King",
-    511,
-    STATUS.QUEUE,
-    0,
-    "horror"
-);
-const atomicHabits = new Book(
-    "Atomic Habits",
-    "James Clear",
-    489,
-    STATUS.QUEUE,
-    100,
-    "personal development"
-);
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 256, STATUS.IN_PROGRESS, 256, "fantasy");
+const theShining = new Book("The Shining", "Stephen King", 511, STATUS.QUEUE, 0, "horror");
+const atomicHabits = new Book("Atomic Habits", "James Clear", 489, STATUS.IN_PROGRESS, 100, "personal development");
 
-// const cardLarge = createLargeCard(atomicHabits);
-// const inProgressList = document.querySelector(".active-books");
-// inProgressList.append(cardLarge);
-// console.log(theHobbit.updateProgress(10));
-
-const displayInProgreesBooks = (book) => {
-    const cardLarge = createLargeCard(book);
-    inProgressList.append(cardLarge);
+const displayInProgressBooks = (book) => {
+	const cardLarge = createLargeCard(book);
+	inProgressList.append(cardLarge);
 };
-
-displayInProgreesBooks(atomicHabits);
-displayInProgreesBooks(theHobbit);
-displayInProgreesBooks(theShining);
 
 library.addToBook(theHobbit);
 library.addToBook(theShining);
 library.addToBook(atomicHabits);
 console.log(library.getBooks());
-library.removeBook(theHobbit);
 console.log("-------------");
 console.log(library.getBooks());
 console.log(library.getCategoryOfBooks("development"));
@@ -140,6 +100,10 @@ console.log("-------------");
 console.log(library.searchBook("ato"));
 console.log(library.searchBook("king"));
 console.log(library.searchBook("ho"));
-console.log(`${theHobbit.getPercentRead()}%`);
 console.log(theHobbit.updateProgress(200));
-console.log(`${theHobbit.getPercentRead()}%`);
+
+library.bookList.forEach((book) => {
+	if (book.status === STATUS.IN_PROGRESS) {
+		displayInProgressBooks(book);
+	}
+});
